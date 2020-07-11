b107f7b02fa01fe7ecd2b6152e983b2fd9175851
OndraK <ondrej.kajinek@gmail.com>
2020-07-12 01:31:21 +0200

    Added test for non-existing offer history

---
+++ tests/api/product/test_history.py
@@ -0,0 +1,16 @@
+"""Test suite for PUT /api/offer/{pk}/history endpoint."""
+
+# third-party
+from django.urls import reverse
+import pytest   # pylint: disable=import-error
+
+
+@pytest.mark.django_db
+def test_offer_history_not_found(api_client):
+    """Test retrieving non-existing history."""
+    api_response = api_client.get(
+        reverse("offer-history", kwargs={"pk": 1}),
+        {},
+        format="json"
+    )
+    assert api_response.status_code == 404
