4297f30ca2711341f7d2d6cb430a8f4e70d228ea
OndraK <ondrej.kajinek@gmail.com>
2020-07-12 18:22:51 +0200

    minor refactoring in tests

--- tests/api/product/test_product_update.py
+++ tests/api/product/test_product_update.py
@@ -8,6 +8,19 @@ import pytest   # pylint: disable=import-error
 from api.product.models import Product
 
 
+def _create_and_update_product(no_product_register, api_client, product_data):
+    """Create and update Product."""
+    with no_product_register():
+        product = Product.objects.create(**product_data["input"])
+
+    response = api_client.put(
+        reverse("product-detail", kwargs={"pk": product.id}),
+        product_data["change"],
+        format="json"
+    )
+    return product, response
+
+
 @pytest.mark.django_db
 def test_product_update_not_found(api_client):
     """Update non-existing product."""
@@ -24,13 +37,8 @@ def test_product_update_invalid(
         test_data, fix_structure, no_product_register, api_client
 ):
     """Update existing product with invalid data."""
-    with no_product_register():
-        product = Product.objects.create(**test_data["input"])
-
-    api_response = api_client.put(
-        reverse("product-detail", kwargs={"pk": product.id}),
-        test_data["change"],
-        format="json"
+    _, api_response = _create_and_update_product(
+        no_product_register, api_client, test_data
     )
     assert api_response.status_code == 400
     assert fix_structure(api_response.data) == test_data["error"]
@@ -39,13 +47,8 @@ def test_product_update_invalid(
 @pytest.mark.django_db
 def test_product_update_valid(test_data, no_product_register, api_client):
     """Update existing product with valid data."""
-    with no_product_register():
-        product = Product.objects.create(**test_data["input"])
-
-    api_response = api_client.put(
-        reverse("product-detail", kwargs={"pk": product.id}),
-        test_data["change"],
-        format="json"
+    product, api_response = _create_and_update_product(
+        no_product_register, api_client, test_data
     )
     assert api_response.status_code == 200
     product = Product.objects.get(pk=product.id)
